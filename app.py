cc4249e9b4f147f6543457b85dc7051787e7f3fa
kamuina jeremie <jrmkams@gmail.com>
2024-02-16 11:15:42 +0100

    redirection sur les pages

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/success')
+def success():
+    data = {
+        'message': 'OK',
+        'http_code': 200
+    }
+    return jsonify(data)
 @app.route('/login', methods=['POST'])
 def login():
     # Récupérer les données du formulaire
@@ -13,10 +21,10 @@ def login():
     # Vérifier les informations de connexion
     if username == 'kams' and password == '123456':
         # Redirection vers la page souhaitée
-        return redirect(url_for('dashboard'))
+        return render_template('dashboard.html')
     else:
         # Redirection vers une page d'erreur
-        return redirect(url_for('error'))
+        return render_template('error.html')
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
@@ -25,4 +33,4 @@ def dashboard():
 def error():
     return render_template('error.html')
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port='5000')
